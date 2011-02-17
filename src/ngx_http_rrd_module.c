38b72fe9079ac6b9dda09c1aaeba2483628236d5
Antoine Bonavita <antoine.bonavita@gmail.com>
2011-02-17 16:35:18 +0100

    Basic infrastructure works. Still to plug RRD.

--- src/ngx_http_rrd_module.c
+++ src/ngx_http_rrd_module.c
@@ -17,6 +17,8 @@
 static char *ngx_http_rrd_post_command(ngx_conf_t *cf, void *data, void *conf);
 static ngx_int_t ngx_http_rrd_handler(ngx_http_request_t *r);
 static void *ngx_http_rrd_create_loc_conf(ngx_conf_t *conf);
+static ngx_int_t ngx_http_rrd_update_database(ngx_http_request_t *r);
+static ngx_int_t ngx_http_rrd_show_graph(ngx_http_request_t *r);
 
 /* Module declaration */
 static ngx_http_module_t  ngx_http_rrd_module_ctx = {
@@ -29,7 +31,7 @@ static ngx_http_module_t  ngx_http_rrd_module_ctx = {
     NULL,                          /* create server configuration */
     NULL,                          /* merge server configuration */
 
-    ngx_http_rrd_create_loc_conf,                          /* create location configuration */
+    ngx_http_rrd_create_loc_conf,  /* create location configuration */
     NULL                           /* merge location configuration */
 };
 
@@ -37,7 +39,7 @@ static ngx_http_module_t  ngx_http_rrd_module_ctx = {
 
 /* Structure storing configuration specific to this module. */
 typedef struct {
-	ngx_str_t db_name;
+    ngx_str_t db_name; /* Name of rrd database. */
 } ngx_http_rrd_module_conf_t;
 /* The module configuration creation function. */
 static void *ngx_http_rrd_create_loc_conf(ngx_conf_t *conf)
@@ -54,7 +56,7 @@ static void *ngx_http_rrd_create_loc_conf(ngx_conf_t *conf)
 
 /* Commands offered by this module. */
 static ngx_conf_post_t ngx_http_rrd_post = {
-		ngx_http_rrd_post_command
+        ngx_http_rrd_post_command
 };
 static ngx_command_t  ngx_http_rrd_commands[] = {
 
@@ -83,8 +85,9 @@ ngx_module_t  ngx_http_rrd_module = {
     NGX_MODULE_V1_PADDING
 };
 
-/*  Called as post processing the command rrd. The argument
- * is handled by ngx_conf_st_str_slot. */
+/*  Called as post processing the command "rrd". The argument
+ * of this command is handled by ngx_conf_set_str_slot, then this
+ * function is called. */
 static char*
 ngx_http_rrd_post_command(ngx_conf_t *cf, void *data, void *conf)
 {
@@ -101,54 +104,205 @@ ngx_http_rrd_post_command(ngx_conf_t *cf, void *data, void *conf)
  * the important thing returned is the status (this is a REST-like API).
  */
 static ngx_str_t OK_MSG =
-		ngx_string("You make the rock-n-roll go round, Robin.");
-/*static ngx_str_t ERROR_MSG = ngx_string("You lose.");*/
+        ngx_string("You make the rock-n-roll go round, Robin.");
+#define ERR_BAD_METHOD_MSG_CSTR "rrd module supports only GET and POST verbs."
+static ngx_str_t ERR_BAD_METHOD_MSG =
+        ngx_string(ERR_BAD_METHOD_MSG_CSTR);
+static ngx_str_t ERR_BAD_CONTENT_TYPE_MSG =
+        ngx_string("rrd module supports only application/x-www-form-urlencoded \
+                content type for now.");
+static ngx_str_t IMAGE_PNG = ngx_string("image/png");
+static ngx_str_t TEXT_PLAIN = ngx_string("text/plain");
+static ngx_str_t WWW_FORM_URLENCODED =
+        ngx_string("application/x-www-form-urlencoded");
 
-ngx_buf_t * ngx_http_rrd_create_buf_from_str(ngx_pool_t *pool, ngx_str_t s)
+/*
+ * Helper function to create a chain from an array of ngx_str.
+ */
+ngx_chain_t *ngx_http_rrd_create_chain(ngx_pool_t *pool,
+                                  ngx_uint_t sarray_len, ngx_str_t **sarray)
 {
-	ngx_buf_t *buf;
-	buf = ngx_calloc_buf(pool);
-	if (NULL == buf) {
-		return  NULL;
-	}
-	buf->start = s.data;
-	buf->end = s.data + s.len;
-	buf->pos = buf->start;
-	buf->last = buf->end;
-	buf->memory = 1;
-	return buf;
+    /* Allocate sarray_len chain links */
+    ngx_chain_t *out_chain = ngx_pcalloc(pool, sarray_len * sizeof(ngx_chain_t));
+    if (NULL == out_chain) {
+        return NULL;
+    }
+    /* Allocate sarray_len buffers */
+    ngx_buf_t *buf = ngx_pcalloc(pool, sarray_len * sizeof(ngx_buf_t));
+    if (NULL == buf) {
+        /* Don't try to free memory, nginx will do it when throwing away the
+         * pool.
+         */
+        return NULL;
+    }
+
+    ngx_uint_t i;
+    for (i = 0; i<sarray_len - 1; i++){
+        out_chain[i].buf = &(buf[i]);
+        out_chain[i].next = &out_chain[i+1];
+        buf[i].start = buf[i].pos = sarray[i]->data;
+        buf[i].end = buf[i].last = sarray[i]->data + sarray[i]->len;
+        buf[i].memory = 1;
+    }
+    /* Last one is slightly different. */
+    out_chain[i].buf = &(buf[i]);
+    out_chain[i].next = NULL; /* diff */
+    buf[i].start = buf[i].pos = sarray[i]->data;
+    buf[i].end = buf[i].last = sarray[i]->data + sarray[i]->len;
+    buf[i].memory = 1;
+    buf[i].last_in_chain = 1;
+    buf[i].last_buf = 1;
+    return out_chain;
+}
+/*
+ * Helper function to send an array of ngx_str as response to a request.
+ */
+ngx_uint_t ngx_http_rrd_output_200(ngx_http_request_t *r,
+                                  ngx_uint_t sarray_len, ngx_str_t **sarray)
+{
+    ngx_chain_t *out_chain = ngx_http_rrd_create_chain(r->pool,
+                                                       sarray_len, sarray);
+    if (NULL == out_chain) {
+        /* nothing else I can do... */
+        ngx_log_error(NGX_LOG_ALERT, log, 0,
+                              "memory alloc pb @ngx_http_rrd_body_received");
+        return NGX_HTTP_INTERNAL_SERVER_ERROR;
+    }
+    ngx_uint_t rc;
+    r->headers_out.status = NGX_HTTP_OK;
+    rc = ngx_http_send_header(r);
+    if (rc != NGX_OK) {
+        ngx_log_error(NGX_LOG_ALERT, log, 0,
+                              "pb sending header @ngx_http_rrd_body_received");
+        return NGX_HTTP_INTERNAL_SERVER_ERROR;
+    }
+
+    return ngx_http_output_filter(r, out_chain);
 }
 /* The actual handler that will process requests. */
-static ngx_int_t
+ngx_int_t
 ngx_http_rrd_handler(ngx_http_request_t *r)
 {
-	ngx_int_t ok_tmp;
-	ngx_buf_t *buf;
+    ngx_log_t                 *log;
+    log = r->connection->log;
 
-	r->headers_out.status = NGX_HTTP_OK;
-	ok_tmp = ngx_http_send_header(r);
-	if (ok_tmp != NGX_OK) {
-		return ok_tmp;
-	}
-	/* Create buffer for message */
-	buf = ngx_http_rrd_create_buf_from_str(r->pool, OK_MSG);
-	if (NULL==buf) {return NGX_HTTP_INTERNAL_SERVER_ERROR;}
 
-	ngx_chain_t out_chain;
-	out_chain.buf = buf;
+    if (NGX_HTTP_GET == r->method) {
+        return ngx_http_rrd_show_graph(r);
+    } else if (NGX_HTTP_POST == r->method) {
+        return ngx_http_rrd_update_database(r);
+    } else if (NGX_HTTP_HEAD == r->method) {
+        /*  HEAD is supposed to give you the headers the GET would give you.
+         * So, we're providing the content-type.
+         */
+        r->headers_out.status = NGX_HTTP_OK;
+        r->header_only = 1;
+        r->headers_out.content_type.data = IMAGE_PNG.data;
+        r->headers_out.content_type.len = IMAGE_PNG.len;
+        return ngx_http_send_header(r);
+    } else {
+        ngx_log_error(NGX_LOG_ALERT, log, 0,
+                              ERR_BAD_METHOD_MSG_CSTR);
+        ngx_http_complex_value_t cv = {ERR_BAD_METHOD_MSG, NULL, NULL, NULL};
 
-	ngx_http_rrd_module_conf_t *rrd_conf;
-	rrd_conf = ngx_http_get_module_loc_conf(r, ngx_http_rrd_module);
-	buf = ngx_http_rrd_create_buf_from_str(r->pool, rrd_conf->db_name);
-	if (NULL==buf) {return NGX_HTTP_INTERNAL_SERVER_ERROR;}
-	buf->last_in_chain = 1;
-	buf->last_buf = 1;
-	ngx_chain_t next;
-	out_chain.next = &next;
-	next.buf = buf;
-	next.next = NULL;
+        return ngx_http_send_response(r, NGX_HTTP_NOT_ALLOWED,
+                                      &TEXT_PLAIN, &cv);
+    }
+}
+/*
+ *  The handler for POST requests (that update the RRD database). The
+ * thing here is to remember that when this is called, the body might
+ * not be available. So, you need to register an extra callback that
+ * will be called when the body is available.
+ */
+void ngx_http_rrd_body_received(ngx_http_request_t *r);
+ngx_int_t ngx_http_rrd_update_database(ngx_http_request_t *r)
+{
+    ngx_log_t *log = r->connection->log;
+    ngx_int_t rc;
 
-	return ngx_http_output_filter(r, &out_chain);
+    if (r->headers_in.content_type == NULL
+            || r->headers_in.content_type->value.data == NULL
+            || r->headers_in.content_type->value.len != WWW_FORM_URLENCODED.len
+            || ngx_strncasecmp(r->headers_in.content_type->value.data,
+                    (u_char *) WWW_FORM_URLENCODED.data,
+                    WWW_FORM_URLENCODED.len) != 0)
+    {
+        ngx_log_error(NGX_LOG_ALERT, log, 0,
+                              (char *) ERR_BAD_CONTENT_TYPE_MSG.data);
+        ngx_http_complex_value_t cv = {ERR_BAD_CONTENT_TYPE_MSG, NULL, NULL, NULL};
+
+        return ngx_http_send_response(r, NGX_HTTP_NOT_ALLOWED,
+                                      &TEXT_PLAIN, &cv);
+    }
+    ngx_log_debug(NGX_LOG_DEBUG_HTTP, log, 0,
+                          "rrd module: Content-type is OK. Proceeding.");
+
+    ngx_log_debug(NGX_LOG_DEBUG_HTTP, log, 0,
+                   "rrd module: start reading client request body");
+
+    rc = ngx_http_read_client_request_body(r, ngx_http_rrd_body_received);
+
+    if (rc == NGX_ERROR || rc >= NGX_HTTP_SPECIAL_RESPONSE) {
+        return rc;
+    }
+
+    if (rc == NGX_AGAIN) {
+        /* Don't call me again, but call the body_received. */
+        return NGX_DONE;
+    }
+    if (NGX_OK == rc) {
+        ngx_log_debug(NGX_LOG_DEBUG_HTTP, log, 0,
+                      "rrd module: client request body already read");
+        return rc;
+    }
+
+    ngx_log_error(NGX_LOG_ALERT, log, 0,
+                   "rrd module: unexpected response code from"
+                   "ngx_http_read_client_request_body : %u", rc);
+    return NGX_HTTP_INTERNAL_SERVER_ERROR;
+}
+/*
+ *  Called when the full body has been received. Even if the body is sent in
+ *  multiple chinks, this will be called only once when everything has
+ *  been received.
+ */
+void ngx_http_rrd_body_received(ngx_http_request_t *r)
+{
+    ngx_log_t *log = r->connection->log;
+
+    ngx_chain_t *body_chain = r->request_body->bufs;
+    /* Use the content length as a mw for our value. */
+    u_char* copy_idx;
+    u_char* rrd_value = copy_idx =
+            ngx_palloc(r->pool, r->headers_in.content_length_n);
+    u_char* p;
+    ngx_int_t looking_for_eq = 1;
+    do {
+        p = body_chain->buf->start;
+        while (p!=body_chain->buf->end && looking_for_eq) {
+            if ('=' == *(p++)) {
+                looking_for_eq = 0;
+            }
+        }
+        ngx_memcpy(rrd_value, p, body_chain->buf->end - p);
+        body_chain = body_chain->next;
+    } while (NULL != body_chain);
+
+
+    ngx_http_rrd_module_conf_t *rrd_conf;
+    rrd_conf = ngx_http_get_module_loc_conf(r, ngx_http_rrd_module);
+
+    ngx_str_t* out_str[] = {&OK_MSG, &(rrd_conf->db_name)};
+    ngx_http_rrd_output_200(r, 2, out_str);
 }
 
+static ngx_int_t ngx_http_rrd_show_graph(ngx_http_request_t *r)
+{
+    ngx_http_rrd_module_conf_t *rrd_conf;
+    rrd_conf = ngx_http_get_module_loc_conf(r, ngx_http_rrd_module);
+
+    ngx_str_t* out_str[] = {&OK_MSG, &(rrd_conf->db_name)};
+    return ngx_http_rrd_output_200(r, 2, out_str);
+}
 

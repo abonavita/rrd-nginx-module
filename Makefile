1f1f5542295af352cbbf8fbe33c0fcccf68e9fb9
Antoine Bonavita <antoine.bonavita@gmail.com>
2011-02-17 16:34:52 +0100

    Initial revision : accepts requests and sends OK pages. Tests include POST body buffering.

---
+++ Makefile
@@ -0,0 +1,2 @@
+all:
+	cd ../nginx-0.8.53; make; cd ..
---
+++ test.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python
+import httplib
+import urllib
+import unittest
+import time
+
+class TestMethods(unittest.TestCase):
+
+    def test_PUT(self):
+        conn = httplib.HTTPConnection("localhost:8000")
+        params = urllib.urlencode({'name': 'daniel'});
+        conn.request("PUT", "/tutu", params)
+        response = conn.getresponse()
+        self.assertEqual(response.status, 405)
+        data = response.read();
+        conn.close()
+        self.assertRegexpMatches(data, ".*support.*GET.*POST");
+
+    def test_HEAD(self):
+        conn = httplib.HTTPConnection("localhost:8000")
+        conn.request("HEAD", "/tutu")
+        response = conn.getresponse()
+        self.assertEqual(response.status, 200)
+        self.assertEqual(response.getheader("Content-Type"), "image/png")
+        conn.close()
+
+    def test_POST_bad_content_type(self):
+        conn = httplib.HTTPConnection("localhost:8000")
+        params = "name=daniel"
+        headers = {"Content-type": "text/plain",
+                   "Accept": "text/plain"}
+        conn.request("POST", "/tutu", params, headers)
+        response = conn.getresponse()
+        self.assertEqual(response.status, 405)
+        data = response.read();
+        conn.close()
+        self.assertRegexpMatches(data, ".*content type.*");
+
+    def test_POST(self):
+        conn = httplib.HTTPConnection("localhost:8000")
+        params = urllib.urlencode({'value' : 'N:12:34:56:78'})
+        headers = {"Content-type": "application/x-www-form-urlencoded",
+                   "Accept": "text/plain"}
+        conn.request("POST", "/tutu", params, headers)
+        response = conn.getresponse()
+        self.assertEqual(response.status, 200)
+        data = response.read();
+        conn.close()
+        self.assertRegexpMatches(data, ".*Robin.*");
+
+    def test_POST_BIG(self):
+        conn = httplib.HTTPConnection("localhost", 8000, None, 20)
+        params = urllib.urlencode({'value' : 'N:12:34:56:78' * 10000})
+        conn.putrequest("POST", "/tutu")
+        conn.putheader('Content-Type', "application/x-www-form-urlencoded")
+        conn.putheader('Content-Length', str(len(params) * 2))
+        conn.putheader('Accept', "text/plain")
+        conn.endheaders()
+        time.sleep(4)
+        conn.send(params)
+        time.sleep(4)
+        conn.send(params)
+        response = conn.getresponse()
+        self.assertEqual(response.status, 200)
+        data = response.read();
+        conn.close()
+        self.assertRegexpMatches(data, ".*Robin.*");
+
+if __name__ == '__main__':
+    unittest.main()
